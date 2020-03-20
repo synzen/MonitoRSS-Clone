98bed647e51584d7274be29fc71e8c42ccdd3fcb
synzen <michael994664@gmail.com>
2020-03-19 21:59:13 -0400

    Check if config.json exists before reading

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 const path = require('path')
 const DiscordRSS = require('discord.rss')
 const schedulesPath = path.join(__dirname, 'settings', 'schedules.json')
-const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'settings', 'config.json')))
+const configPath = path.join(__dirname, 'settings', 'config.json')
+const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(path.join(__dirname, 'settings', 'config.json'))) : {}
 const schedules = fs.existsSync(schedulesPath) ? JSON.parse(fs.readFileSync(schedulesPath)) : {}
 
 const clientManager = new DiscordRSS.ClientManager({
