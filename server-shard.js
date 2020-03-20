63a989a99db6d955ce1357de80fa585eb80c8609
synzen <michael994664@gmail.com>
2020-03-19 20:28:07 -0400

    Pass config to ClientManager

--- server-shard.js
+++
@@ -1,6 +0,0 @@
-// Create a single client
-
-const DiscordRSS = require('discord.rss')
-const drss = new DiscordRSS.Client()
-
-drss.login(process.env.DRSS_BOT_TOKEN)
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const fs = require('fs')
 const path = require('path')
 const DiscordRSS = require('discord.rss')
 const schedulesPath = path.join(__dirname, 'settings', 'schedules.json')
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'settings', 'config.json')))
 const schedules = fs.existsSync(schedulesPath) ? JSON.parse(fs.readFileSync(schedulesPath)) : {}
 
 const clientManager = new DiscordRSS.ClientManager({
   setPresence: true,
-  schedules
+  schedules,
+  config
 })
 
 clientManager.run()
