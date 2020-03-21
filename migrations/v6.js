83032b27e81bf43970e8be776a1ac1e96100dfe7
synzen <michael994664@gmail.com>
2020-03-20 22:27:24 -0400

    Update migration script

--- migrations/v6.js
+++ migrations/v6.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const DiscordRSS = require('discord.rss')
 const config = require('../settings/config.json')
 
@@ -7,8 +8,17 @@ DiscordRSS.validateConfig(config)
 const v6 = DiscordRSS.migrations.v6
 
 v6(config)
-  .then(() => {
+  .then((failures) => {
     console.log('Finished v6 migration')
+    if (failures.length === 0) {
+      return
+    }
+    for (const item of failures) {
+      console.log(item.error)
+      console.log(JSON.stringify(item.data, null, 2))
+    }
+    fs.writeFileSync(`./migrations/failures.log`, JSON.stringify(failures, null, 2))
+    console.error('\n\n\x1b[31mThere were some migration failures. See output above, or failures.log in the migrations directory. ')
   })
   .catch(err => {
     console.error(err)
