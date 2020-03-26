cca6388828f51787ddbe3e9735330e7113fafc6b
synzen <michael994664@gmail.com>
2020-03-26 18:27:05 -0400

    Change run to start

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const DiscordRSS = require('discord.rss')
 const schedulesPath = path.join(__dirname, 'settings', 'schedules.json')
 const configPath = path.join(__dirname, 'settings', 'config.json')
-const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(path.join(__dirname, 'settings', 'config.json'))) : {}
+const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(configPath)) : {}
 const schedules = fs.existsSync(schedulesPath) ? JSON.parse(fs.readFileSync(schedulesPath)) : {}
 
 const clientManager = new DiscordRSS.ClientManager({
@@ -12,4 +12,4 @@ const clientManager = new DiscordRSS.ClientManager({
   config
 })
 
-clientManager.run()
+clientManager.start()
