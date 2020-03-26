1bba78fe384a386b8d9d6fc431fb3ac6c3ce511b
synzen <michael994664@gmail.com>
2020-03-26 18:28:21 -0400

    Rename config.json to config.bot.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const DiscordRSS = require('discord.rss')
 const schedulesPath = path.join(__dirname, 'settings', 'schedules.json')
-const configPath = path.join(__dirname, 'settings', 'config.json')
+const configPath = path.join(__dirname, 'settings', 'config.bot.json')
 const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(configPath)) : {}
 const schedules = fs.existsSync(schedulesPath) ? JSON.parse(fs.readFileSync(schedulesPath)) : {}
 
