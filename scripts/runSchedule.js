63ec9c0cf16d284bee7e885777a700484fe5e29a
synzen <michael994664@gmail.com>
2020-05-06 23:08:56 -0400

    Log failed URLs in runSchedule script

--- scripts/runSchedule.js
+++ scripts/runSchedule.js
@@ -6,5 +6,8 @@ const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(configPath
 const runSchedule = require('discord.rss').scripts.runSchedule
 
 runSchedule(config)
+  .then((scheduleRun) => {
+    console.log(scheduleRun.failedURLs)
+  })
   .catch(console.error)
 
