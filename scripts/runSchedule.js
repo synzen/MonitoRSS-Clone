70376a6705bf90ec9483ac791c67bf35990e521e
synzen <michael994664@gmail.com>
2020-05-07 01:04:38 -0400

    Force script to stay alive with setInterval

--- scripts/runSchedule.js
+++ scripts/runSchedule.js
@@ -2,29 +2,36 @@ const fs = require('fs')
 const path = require('path')
 const configPath = path.join(__dirname, '..', 'settings', 'config.bot.json')
 const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(configPath)) : {}
-
 const { scripts, FeedFetcher } = require('discord.rss')
 
-scripts.runSchedule(config, (err, scheduleRun) => {
-  if (err) {
-    throw err
-  }
-  const failedURLs = scheduleRun.failedURLs
-  let done = 0
-  const falsePositives = []
-  console.log('Failed URLs', scheduleRun.failedURLs)
-  console.log('Checking for false positives...')
-  for (const url of failedURLs) {
-    FeedFetcher.fetchFeed(url)
-      .then(() => {
-        falsePositives.push(url)
-      })
-      .catch(err => {})
-      .finally(() => {
-        if (++done === failedURLs.size) {
-          console.log(`Done checking URLs, false positives (${falsePositives.length}):`, falsePositives)
-        }
-      })
-  }
-})
+const stayAlive = setTimeout(() => {}, 1 << 30)
 
+/**
+ * @param {string[]} urls 
+ */
+async function checkFalsePositives (urls) {
+  console.log(`Failed URLs (${urls.length})`, urls)
+  try {
+    const results = await Promise.allSettled(urls.map(url => FeedFetcher.fetchFeed(url)))
+    const fp = []
+    for (let i = 0; i < urls.length; ++i) {
+      const url = urls[i]
+      const result = results[i]
+      if (result.status === 'fulfilled') {
+        fp.push(url)
+      }
+    }
+    console.log(`Done checking URLs, false positives (${fp.length}):`, fp)
+    clearInterval(stayAlive)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+scripts.runSchedule(config)
+  .then((scheduleRun) => {
+    scheduleRun.on('finish', () => {
+      checkFalsePositives(Array.from(scheduleRun.failedURLs))
+    })
+  })
+  .catch(console.error)
