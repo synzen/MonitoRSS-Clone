06e8537d6f3383549d1e74b6af110aeda790e2cc
synzen <michael994664@gmail.com>
2021-05-15 00:56:45 -0400

    Adjust runSchedule to run deliver articles independently

--- scripts/runSchedule.js
+++ scripts/runSchedule.js
@@ -2,7 +2,26 @@ const fs = require('fs')
 const path = require('path')
 const configPath = path.join(__dirname, '..', 'settings', 'config.bot.json')
 const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(configPath)) : {}
-const { scripts } = require('monitorss')
+const { scripts, DeliveryPipeline } = require('monitorss')
+const { RESTProducer } = require('@synzen/discord-rest')
 
-scripts.runSchedule(config)
+const {
+    apis: {
+        discordHttpGateway: {
+        redisUri: serviceRedisUri
+        }
+    }
+} = config
+
+const producer = new RESTProducer(serviceRedisUri)
+const deliveryPipeline = new DeliveryPipeline(null, producer)
+
+scripts.runSchedule(config).then((scheduleManager) => {
+    scheduleManager.on('newArticle', newArticle => {
+        deliveryPipeline.deliver(newArticle, null, true).catch((err) => {
+            console.error(`Failed to deliver article ${JSON.stringify(newArticle)}`, err)
+        })
+    })
+})
+
   
